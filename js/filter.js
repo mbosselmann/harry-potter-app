f438bb7a654b1a20b1b93b09c7ef0a5f0373d45e
Mareike Boßelmann <mareike.bosselmann@gmx.de>
2021-09-12 21:40:12 +0200

    Add filter button with icons

--- js/filter.js
+++ js/filter.js
@@ -14,9 +14,44 @@
 
 export function setFilter() {
   const filterContainer = document.createElement('div')
+  filterContainer.classList.add('filter-container')
   document.getElementById('filter').appendChild(filterContainer)
 
+  const filterContainerHeader = document.createElement('div')
+  filterContainerHeader.classList.add('filter-container-header')
+  filterContainer.appendChild(filterContainerHeader)
+
   const filterTitle = document.createElement('h2')
+  filterTitle.classList.add('filter-title')
   filterTitle.textContent = 'Filter'
-  filterContainer.appendChild(filterTitle)
+  filterContainerHeader.appendChild(filterTitle)
+
+  const filterButtonEl = document.createElement('button')
+  filterButtonEl.classList.add('expand-filter-button')
+  filterContainerHeader.appendChild(filterButtonEl)
+
+  const moreButtonImg = document.createElement('img')
+  moreButtonImg.setAttribute('src', '/images/expand-more.png')
+  moreButtonImg.classList.add('expand-more-img')
+  filterButtonEl.appendChild(moreButtonImg)
+
+  //   const sectionMainInformation = document.createElement('div')
+  //   sectionMainInformation.classList.add('section-content--hidden')
+  //   filterContainer.appendChild(sectionMainInformation)
+
+  filterButtonEl.addEventListener('click', () => {
+    if (sectionMainInformation.classList.contains('section-content--hidden')) {
+      sectionMainInformation.classList.remove('section-content--hidden')
+      moreButtonImg.removeAttribute('src')
+      moreButtonImg.setAttribute('src', '/images/expand-less.png')
+      filterButtonEl.classList.remove('expand-filter-button')
+      filterButtonEl.classList.add('expand-less-filter-button')
+    } else {
+      sectionMainInformation.classList.add('section-content--hidden')
+      moreButtonImg.removeAttribute('src')
+      moreButtonImg.setAttribute('src', '/images/expand-more.png')
+      filterButtonEl.classList.remove('expand-less-filter-button')
+      filterButtonEl.classList.add('expand-filter-button')
+    }
+  })
 }
